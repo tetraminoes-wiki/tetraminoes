2ef2bcaf39184e815aad7defb95fdde10bd65707
RunTheBot <58890327+RunTheBot@users.noreply.github.com>
2023-12-17 13:54:50 -0500

    switch to jsx instad of remark

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,10 +37,8 @@ const config = {
             ({
                 docs: {
                     sidebarPath: './sidebars.js',
-                    // Please change this to your repo.
-                    // Remove this to remove the "edit this page" links.
                     editUrl: 'https://github.com/tetraminoes/tetramino-wiki/edit/main/',
-                    remarkPlugins: [remarkFumen],
+                    // remarkPlugins: [remarkFumen],
                     path: './docs'
                 },
                 theme: {
