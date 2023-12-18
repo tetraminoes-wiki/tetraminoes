8d2396e0391320982c8c29db238a56d14adf48d8
Bread <86811307+Breadlicia@users.noreply.github.com>
2023-12-18 02:59:28 +0000

    Editing url fixed

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -39,7 +39,7 @@ const config = {
                     sidebarPath: './sidebars.js',
                     // Please change this to your repo.
                     // Remove this to remove the "edit this page" links.
-                    editUrl: 'https://github.com/tetraminoes/tetramino-wiki/edit/main/',
+                    editUrl: 'https://github.com/tetraminoes-wiki/tetraminoes/edit/main/',
                     remarkPlugins: [remarkFumen],
                     path: './docs'
                 },
